4318ef12fe1af8c1fe0eb5dfd04c8b249e1b486a
Keyur Desai <keyurdesai124@gmail.com>
2025-04-21 22:27:41 +0530

    added footer into sign in page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.querySelector('#signIn');
 const container = document.getElementById('container');
+const scrollBox = document.querySelector('section');
+// const breakPoint = document.querySelector('footer')
+// const maxScroll = 800;
+// let lastScroll = 0;
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
@@ -9,3 +13,9 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+    scrollBox.addEventListener('scroll', function() {
+        if (this.scrollTop > 900) {
+          this.scrollTop = 900; // Prevents scrolling beyond 500px
+        }
+      });
